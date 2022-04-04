a6664abaa6534473f5e3eb551550ca43b4d213cd
thangnguyen2 <thangnguyen2@beeketing.net>
2022-04-04 18:37:46 +0700

    abc

--- src/QA/Passwork.java
+++ src/QA/Passwork.java
@@ -24,13 +24,60 @@ public class Passwork {
                 }
             }
             int count = 0;
+            int key = 0;
             for (int k = 0; k < long_pass; k++) {
                 if (data_[0][k] == data_[1][k]) {
                     count++;
                 }
+                if (data_[0][k] == 1 && (data_[1][k] == 2 || data_[1][k] == 4)) {
+                    count++;
+                    key++;
+                }
+                if (data_[0][k] == 2 && (data_[1][k] == 1 || data_[1][k] == 3 || data_[1][k] == 5)) {
+                    count++;
+                    key++;
+                }
+                if (data_[0][k] == 3 && (data_[1][k] == 2 || data_[1][k] == 6)) {
+                    count++;
+                    key++;
+                }
+                if (data_[0][k] == 4 && (data_[1][k] == 1 || data_[1][k] == 5 || data_[1][k] == 7)) {
+                    count++;
+                    key++;
+                }
+                if (data_[0][k] == 5 && (data_[1][k] == 2 || data_[1][k] == 4 || data_[1][k] == 6 || data_[1][k] == 8)) {
+                    count++;
+                    key++;
+                }
+                if (data_[0][k] == 6 && (data_[1][k] == 9 || data_[1][k] == 3 || data_[1][k] == 5)) {
+                    count++;
+                    key++;
+                }
+                if (data_[0][k] == 7 && (data_[1][k] == 4 || data_[1][k] == 8)) {
+                    count++;
+                    key++;
+                }
+                if (data_[0][k] == 8 && (data_[1][k] == 5 || data_[1][k] == 7 || data_[1][k] == 9)) {
+                    count++;
+                    key++;
+                }
+                if (data_[0][k] == 9 && (data_[1][k] == 6 || data_[1][k] == 8)) {
+                    count++;
+                    key++;
+
+                }
+                if (data_[0][k] == 0 && data_[1][k] == 2) {
+                    count++;
+                    key++;
+
+                }
             }
+            System.out.println(count);
+            System.out.println(key);
             boolean min = false;
-            if(count != long_pass){
+
+
+            if (count != long_pass || key > 1) {
                 System.out.println(min);
 
             } else {
